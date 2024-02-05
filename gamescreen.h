f873d56fa6c36bf85b3c4b6227cc365bdb49d9af
MrSteelRat <drebin341@gmail.com>
2024-02-05 15:25:48 +0300

    First build

---
+++ gamescreen.h
@@ -0,0 +1,22 @@
+#ifndef GAMESCREEN_H
+#define GAMESCREEN_H
+
+#include <QDialog>
+
+namespace Ui {
+class GameScreen;
+}
+
+class GameScreen : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit GameScreen(QWidget *parent = nullptr);
+    ~GameScreen();
+
+private:
+    Ui::GameScreen *ui;
+};
+
+#endif // GAMESCREEN_H
